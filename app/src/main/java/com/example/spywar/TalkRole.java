4562e2d2aaaa05367d511f72a5308840b44e5398
serim53 <tpfla9953@naver.com>
2021-02-11 23:40:20 +0900

    0211 func

--- app/src/main/java/com/example/spywar/TalkRole.java
+++ app/src/main/java/com/example/spywar/TalkRole.java
@@ -1,6 +1,10 @@
 package com.example.spywar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +22,7 @@ public class TalkRole extends AppCompatActivity{
         setContentView(R.layout.activity_talkrole);
         TextView roletext = (TextView) findViewById(R.id.roletext);
         TextView missiontext = (TextView) findViewById(R.id.missiontext);
-
+        Button nextbtn = (Button) findViewById(R.id.nextbtn);
         // talknumber에서 게임을 진행하는 인원수를 받아옴
         Bundle intent = getIntent().getExtras();
 
@@ -34,12 +38,18 @@ public class TalkRole extends AppCompatActivity{
 
         citizennum = totalnum-spynum-1; // 시민수 = 전체수-스파이수-사회자1인
 
-        ArrayList roles = new ArrayList<>();
+        ArrayList<String> roles = new ArrayList<String>();
         roles.add("사회자");
         for(int i=0;i<spynum;i++)
             roles.add("SPY");
         for(int i=0;i<citizennum;i++)
             roles.add("시민");
+
+        //역할 셔플
+        Collections.shuffle(roles);
+
+        String[] role = (String[]) roles.toArray(new String[roles.size()]);
+
 
         // 시민 미션 초기설정
         String[] citizenmission = {"1","2","3","4","5","6"};
@@ -50,5 +60,21 @@ public class TalkRole extends AppCompatActivity{
         String[] spymission = {"1","2","3","4","5","6"};
         // 미션 셔플(매번 랜덤으로 각 스파이마다 미션이 다르게 나오기 위함)
         Collections.shuffle(Arrays.asList(spymission));
+        String myrole;
+        String mymission;
+
+
+        for(int i=0; i<totalnum; i++){
+
+            nextbtn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+
     }
 }
